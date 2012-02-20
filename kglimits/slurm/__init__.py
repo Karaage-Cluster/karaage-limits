Karaage-Cluster/karaage-limits

commit 238884c33ed457484ca37adb6c6ed4e9dbbf9113

Make Gold and Slurm settings configurable.

kglimits/slurm/__init__.py
@@ -5,8 +5,15 @@ from datetime import datetime
 import subprocess
 import csv
 
-slurm_path = "/usr/local/slurm/latest/bin/sacctmgr"
-slurm_default_project = "default"
+from django.conf import settings
+
+if not hasattr(settings, 'SLURM_PATH'):
+    settings.SLURM_PATH = "/usr/local/slurm/bin"
+if not hasattr(settings, 'SLURM_DEFAULT_PROJECT'):
+    settings.SLURM_DEFAULT_PROJECT = "default"
+
+slurm_path = settings.SLURM_PATH
+slurm_default_project = settings.SLURM_DEFAULT_PROJECT
 
 import sys
 logfile = open('/tmp/slurm.log', 'a')

kglimits/gold/__init__.py
@@ -5,8 +5,15 @@ from datetime import datetime
 import subprocess
 import csv
 
-gold_path = "/usr/local/gold/bin"
-gold_default_project = "default"
+from django.conf import settings
+
+if not hasattr(settings, 'GOLD_PATH'):
+    settings.GOLD_PATH = "/usr/local/gold/bin"
+if not hasattr(settings, 'GOLD_DEFAULT_PROJECT'):
+    settings.GOLD_DEFAULT_PROJECT = "default"
+
+gold_path = settings.GOLD_PATH
+gold_default_project = settings.GOLD_DEFAULT_PROJECT
 
 import sys
 logfile = open('/tmp/gold.log', 'a')

commit 39ad99dc9ad7c49581f92fb2c5dfc99d809ad4ed

Fix broken path to slurm was broken in 238884c33ed457484ca37adb6c6ed4e9dbbf9113

kglimits/slurm/__init__.py
@@ -8,7 +8,7 @@ import csv
 from django.conf import settings
 
 if not hasattr(settings, 'SLURM_PATH'):
-    settings.SLURM_PATH = "/usr/local/slurm/bin"
+    settings.SLURM_PATH = "/usr/local/slurm/latest/bin/sacctmgr"
 if not hasattr(settings, 'SLURM_DEFAULT_PROJECT'):
     settings.SLURM_DEFAULT_PROJECT = "default"
 