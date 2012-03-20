78f1a9f3d16f01e85ec94bab0c57c2eda26ee9c8
Brian May <brian@vpac.org>
2012-03-20 14:17:09 +1100

    Have to create the organisation first before trying to add it.

--- kglimits/gold/__init__.py
+++ kglimits/gold/__init__.py
@@ -332,8 +332,8 @@ def project_saved(sender, instance, created, **kwargs):
         # update project meta information
         name = truncate(instance.name, 40)
         call(["gchproject","-d",filter_string(name),"-p",pid])
-        call(["gchproject","-X","Organization=%s"%filter_string(instance.institute.name),"-p",pid])
         call(["goldsh","Organization","Create","Name=%s"%name],ignore_errors[185])
+        call(["gchproject","-X","Organization=%s"%filter_string(instance.institute.name),"-p",pid])
     else:
         # project is deleted
         logger.debug("project is not active")
